9f21a6521f9346634d285fb953f60212236d122c
Adrien Thebo <adrien@lagrange-automation.io>
2020-02-07 22:43:48 -0300

    nfs: add assertions for nfs volume permissions

--- nfs/controls/auxtel_nfs_filesystems.rb
+++ nfs/controls/auxtel_nfs_filesystems.rb
@@ -9,9 +9,21 @@ control "auxtel_nfs_filesystems" do
     its(:percent_free) { should be >= 80.0 }
   end
 
+  describe file("/data/project") do
+    it { should exist }
+    its(:owner) { should eq 'root' }
+    its(:group) { should eq 'nfs-project-admin' }
+  end
+
   describe filesystem("/data/lsstdata") do
     its(:size_kb) { should be >= 1.5 * TERABYTE }
     its(:percent_free) { should be >= 80.0 }
+  end
+
+  describe file("/data/lsstdata") do
+    it { should exist }
+    its(:owner) { should eq 'root' }
+    its(:group) { should eq 'nfs-lsstdata-admin' }
   end
 
   describe filesystem("/data/scratch") do
@@ -22,5 +34,12 @@ control "auxtel_nfs_filesystems" do
   describe filesystem("/data/home") do
     its(:size_kb) { should be >= 0.75 * TERABYTE }
     its(:percent_free) { should be >= 80.0 }
+  end
+
+  describe file("/data/scratch") do
+    it { should exist }
+    its(:owner) { should eq 'root' }
+    its(:group) { should eq 'nfs-scratch-admin' }
+    it { should be_setgid }
   end
 end
