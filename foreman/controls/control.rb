033638e4449927a8d668d7e41ce238fe758f778b
Adrien Thebo <adrien@lagrange-automation.io>
2020-02-11 18:55:02 -0300

    Verify that foreman can virsh connect to core nodes

--- foreman/controls/control.rb
+++ foreman/controls/control.rb
@@ -14,3 +14,17 @@ control 'foreman_proxy' do
     it { should be_enabled }
   end
 end
+
+control 'foreman_virsh' do
+  only_if { command('id').stdout.match(/root/) }
+
+  (1..2).each do |offset|
+    hostname = "core#{offset}"
+    url = "qemu+ssh://foreman@#{hostname}.#{sys_info.domain}/system"
+    describe command("sudo -u foreman virsh --connect #{url} list --all") do
+      its(:exit_status) { should eq 0 }
+      its(:stdout) { should match(/ipa#{offset}.#{sys_info.domain}/) }
+      its(:stdout) { should match(/dns#{offset}.#{sys_info.domain}/) }
+    end
+  end
+end
